97663ba157d668221bcccee1774fdb5d9f774097
szwar <szwar@LAPTOP-18IPGD6N>
2023-05-25 23:10:13 +0200

    Add project files.

---
+++ BlazorFullStackCrud/Client/Services/SuperHeroServices/ISuperHeroService.cs
@@ -0,0 +1,19 @@
+﻿using BlazorFullStackCrud.Client.Pages;
+
+
+namespace BlazorFullStackCrud.Client.Services.SuperHeroServices
+{
+    public interface ISuperHeroService
+    {
+        List<SuperHero> Heroes { get; set; }
+        List<Comic> Comics { get; set; }
+
+        Task GetComics();
+        Task GetSuperHeroes();
+        Task<SuperHero> GetSingleHero(int id);
+        Task CreateHero(SuperHero hero);
+        Task UpdateHero(SuperHero hero);
+        Task DeleteHero(int id);
+
+    }
+}
